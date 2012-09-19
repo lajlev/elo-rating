b318d09e64a2000919177dd06cbb09a32d5a8e58
Michael Lajlev <lajlev@gmail.com>
2012-09-19 08:00:29 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EloRanking::Application.config.secret_token = '6f1d4a91867f98d8b5360be5b13b4b85c98944b2da8147bf763bcf275fbe355fe0471cc01067c0e6038c14c4e215c2a77b03c98ef88a237c493d0d1487f0074e'
