9c03699924362afe07390ae3f6afc55f4cd0a455
Michael Lajlev <lajlev@gmail.com>
2012-09-23 14:54:15 +0200

    Relations commented out

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,3 +1,5 @@
 class Match < ActiveRecord::Base
   attr_accessible :looser_1, :looser_2, :winner_1, :winner_2
+
+  # has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :full_name, :gender, :hashed_password, :matches, :mobile, :points, :salt
 
+  # has_and_belongs_to_many :matches
+
   before_save :default_values
 
   def default_values
